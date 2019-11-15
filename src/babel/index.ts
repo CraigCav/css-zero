d6da5a0d0712f2999c989487a625faf1d0601156
Craig Cavalier <craigcav@gmail.com>
2019-11-14 20:17:43 -0500

    extract CallExpression
    for usage within plugin and macro

--- src/babel/index.ts
+++ src/babel/index.ts
@@ -1,7 +1,6 @@
 import jsx from '@babel/plugin-syntax-jsx';
 import TaggedTemplateExpression from './visitors/TaggedTemplateExpression';
-import isStyles from './utils/isStyles';
-import StyleCache from './StyleCache';
+import CallExpression from './visitors/CallExpression';
 
 export default function cssZeroBabelPlugin(babel) {
   const { types } = babel;
@@ -46,86 +45,7 @@ export default function cssZeroBabelPlugin(babel) {
         },
       },
       CallExpression(path: any, state: any) {
-        if (!isStyles(path)) return;
-
-        const cache = new StyleCache();
-        const args = path.get('arguments');
-
-        args.forEach((arg, i) => {
-          const result = arg.evaluate();
-          const { confident, value } = result;
-
-          if (confident && value) {
-            Object.entries(value).forEach(([key, value]) =>
-              cache.addStyle(key, value)
-            );
-            return;
-          }
-
-          switch (arg.type) {
-            case 'LogicalExpression':
-              if (arg.node.operator !== '&&')
-                throw arg.buildCodeFrameError(
-                  `Styles argument does not support the ${arg.node.operator} operator with dynamic values.`
-                );
-
-              const left = path.get(`arguments.${i}.left`);
-              const right = path.get(`arguments.${i}.right`);
-
-              const valueRight = right.evaluate();
-
-              if (!valueRight.confident)
-                throw arg.buildCodeFrameError(
-                  `Styles argument only accepts boolean expressions in the form "{condition} && {css}".`
-                );
-
-              Object.entries(valueRight.value).forEach(([key, value]) =>
-                cache.addConditionalStyle(key, value, left.node)
-              );
-              return;
-            case 'BooleanLiteral':
-            case 'NullLiteral': {
-              return;
-            }
-            case 'ConditionalExpression':
-            default:
-              return;
-          }
-        });
-
-        state.usage.push(...cache.getUsedClassNames());
-
-        const expressions = cache.getConditionalStyles();
-        const literals = cache.getStyles().join(' ');
-
-        if (!expressions.length && !literals) {
-          path.replaceWith(types.stringLiteral(''));
-          return;
-        }
-
-        if (!expressions.length) {
-          path.replaceWith(types.stringLiteral(literals));
-          return;
-        }
-
-        const concat = (left, right) =>
-          types.binaryExpression('+', left, right);
-
-        path.replaceWith(
-          expressions.reduce(
-            (current: any, { test, consequent, alternate }) => {
-              const node = types.conditionalExpression(
-                test,
-                types.stringLiteral(consequent),
-                types.stringLiteral(alternate)
-              );
-              return current.type === 'NullLiteral'
-                ? node
-                : concat(concat(node, types.stringLiteral(' ')), current);
-            },
-            !literals ? types.nullLiteral() : types.stringLiteral(literals)
-          )
-        );
+        CallExpression(path, state, types);
       },
     },
   };
