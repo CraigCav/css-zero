082d9e515c33c16cdb624d9e75849d27459c56af
Craig Cavalier <craigcav@gmail.com>
2019-11-18 15:17:30 -0500

    update example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import MyStyledThing from './MyStyledThing';
-import logo from './logo.svg';
-import './App.css';
+import {css, styles} from 'css-zero/macro';
+import logoUrl from './logo.svg';
+
+const center = css`
+  text-align: center;
+`;
+
+const logo = css`
+  height: 40vmin;
+`;
+
+const header = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const link = css`
+  color: #09d3ac;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles(center)}>
+      <header className={styles(header)}>
+        <img src={logoUrl} className={styles(logo)} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles(link)}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-        <MyStyledThing>Thing that was styled by css-zero</MyStyledThing>
       </header>
     </div>
   );
